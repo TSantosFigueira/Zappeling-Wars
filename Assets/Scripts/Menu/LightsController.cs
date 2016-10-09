ee514823fc10a5ca3be90c8fc2c92943a40255c3
Thiago Figueira <tsantosfigueira@outlook.com>
2016-10-09 19:57:43 -0400

    Versao 0.3.1.2
    Adicionados:
    -Animação no menu;
    Correções:
    -Câmera segue o jogador sem delays;

---
+++ Assets/Scripts/Menu/LightsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LightsController : MonoBehaviour {
+
+    private SpriteRenderer blink;
+
+	// Use this for initialization
+	void Start () {
+        blink = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        blink.color = new Color(blink.color.r, blink.color.g, blink.color.b, 0);
+        StartCoroutine("TurnOnLights");
+    }
+
+    IEnumerator TurnOnLights()
+    {
+        yield return new WaitForSeconds(1);
+        blink.color = new Color(blink.color.r, blink.color.g, blink.color.b, 255);
+    }
+}
