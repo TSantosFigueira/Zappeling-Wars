9b204bec9b7f232b09701c5bf561ca6b1dc517e0
Thiago Figueira <tsantosfigueira@outlook.com>
2016-10-09 15:47:03 -0400

    0.3.0.7
    Adicionados:
    -Assest finais do menu principal e créditos;
    Correções:
    -Animação dos créditos agora é disposta conforme deve;
    Bugs:
    -Troca de sprites ao clicar botões;
    -Botões de áudio;

---
+++ Assets/Scripts/AboutDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AboutDisplayer : MonoBehaviour {
+
+    public GameObject credits;
+    private Animator creditsAnimator;
+
+	// Use this for initialization
+	void Start () {
+        creditsAnimator = credits.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisplayCredits()
+    {
+        creditsAnimator.SetBool("Display", !creditsAnimator.GetBool("Display"));
+    }
+}
