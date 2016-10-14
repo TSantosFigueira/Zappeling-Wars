18ac263362d4173a3425050aa13293d239cce9cd
XxXnetinhowXxX <albertocorreia2011@gmail.com>
2016-10-13 21:13:57 -0400

    versão 0.3.5.8
    -Adicionado musica nos menus
    -Adicionado Musica no Logo da Empresa
    -Corrigido animação dos botões do Menu

--- Assets/Scripts/Splash Screen/SplashFade.cs
+++ Assets/Scripts/Splash Screen/SplashFade.cs
@@ -13,9 +13,9 @@ public class SplashFade : MonoBehaviour
         splashImage.canvasRenderer.SetAlpha(0.0f);
 
         FadeIn();
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(4f);
         FadeOut();
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(3f);
         SceneManager.LoadScene(loadLevel);
     }
 
